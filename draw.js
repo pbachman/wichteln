ba831bcf5ec61478eaeaacceb9b6f69d4915c6e8
Philipp Bachmann <hi@phil.ch>
2020-11-05 22:17:33 +0100

    Refactored draw logic, added stalemate case.

--- draw.js
+++ draw.js
@@ -6,7 +6,7 @@ let data = JSON.parse(rawdata);
 // sort the array
 shuffle(data);
 
-// draw
+// draw it
 draw(data);
 
 function shuffle(array) {
@@ -16,11 +16,9 @@ function shuffle(array) {
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-    // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
 
-    // And swap it with the current element.
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
@@ -30,35 +28,44 @@ function shuffle(array) {
 }
 
 function draw(array) {
-  let newarray = [];
+  let existing = [];
   let draw = [];
   for (let index = 0; index < array.length; index++) {
     const element = array[index];
     let randomIndex = -1;
     let tries = 0;
     while (randomIndex !== index) {
-      // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * (array.length - 0) + 0);
       if (
         array[index].partner === array[randomIndex].name ||
-        newarray.includes(randomIndex) ||
+        existing.includes(randomIndex) ||
         randomIndex === index
       ) {
         randomIndex = -1;
         tries = tries + 1;
-        console.log(tries);
       } else {
-        newarray.push(randomIndex);
+        existing.push(randomIndex);
         tries = 0;
         break;
       }
+
+      // start again, if it's a stalemate.
+      if (tries > 20) {
+        index = 0;
+        draw = [];
+        existing = [];
+        randomIndex = 0;
+      }
     }
-    //console.log(element.name + " -> " + array[randomIndex].name);
-    draw.push({
-      name: element.name,
-      partner: element.partner,
-      draw: array[randomIndex].name
-    });
+
+    if (array[randomIndex]) {
+      draw.push({
+        name: element.name,
+        partner: element.partner,
+        draw: array[randomIndex].name,
+      });
+    }
   }
-  fs.writeFileSync('draw.json', JSON.stringify(draw));
+  console.table(draw);
+  fs.writeFileSync("draw.json", JSON.stringify(draw));
 }
